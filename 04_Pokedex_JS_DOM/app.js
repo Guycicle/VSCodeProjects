16d6804e3eaf4f8430986247baf79c3dd4e18fdb
Guycicle <guymichaelhepworth@gmail.com>
2023-03-07 11:10:19 +0000

    Using DOM manipulation to create a full index of pokemon using only a few lines of code

---
+++ 04_Pokedex_JS_DOM/app.js
@@ -0,0 +1,18 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector(`#container`)
+const baseURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement(`div`)
+    pokemon.classList = `pokemon`
+    const label = document.createElement(`span`)
+    label.innerText = `#${i}`
+    const newImg = document.createElement(`img`)
+    newImg.src = `${baseURL}${i}.png`
+    container.appendChild(pokemon)
+    pokemon.appendChild(newImg)
+    pokemon.appendChild(label)
+}
